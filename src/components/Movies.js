1b505c7cd3acb414afc93a64d6d6d61ec87aa1f2
charlie763 <cwisoff@gmail.com>
2020-09-28 13:40:37 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+            <h3>{movie.title}</h3>
+            <span>Length: {movie.time}</span>
+            <ul>
+              {movie.genres.map(genre => (
+                <li>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
 
 export default Movies;
+
