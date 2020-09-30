2e7c90cfa0c3539b6262c7ff0eeba00c1af34804
Jonathan Bleibdrey <jonathanbleibdrey@Jonathans-MacBook-Air-2.local>
2020-09-30 15:38:57 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+      <h1>Actors Page</h1>
+      {actors.map( (actors,i) => {
+        return(
+          <div key={i}>
+            <h3>{actors.name}</h3>
+            <ul>
+              {actors.movies.map(movie => <li>{movie}</li>)}
+            </ul>
+          </div>
+        )
+      })}
+      </div>
   );
 };
 
