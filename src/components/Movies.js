8443ca97bcd42b186fd7d555394c1977408bc70e
KaelinAlexander <kaelin.alexander@gmail.com>
2020-10-02 13:19:16 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(item => 
+    <div>
+      {item.title}
+      {item.time}
+     <ul>
+       {item.genres.map(genre =>
+        <li>{genre}</li>)}
+     </ul> 
+    </div>)}
     </div>
   );
 };
