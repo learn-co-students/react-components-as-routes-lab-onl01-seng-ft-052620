5ffe67472469dfc82cac691309f1486ec6acfffc
francepants <franceline90@gmail.com>
2020-10-02 21:39:07 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Title: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map((genres, index) => (
+              <li key={index}>{genres}</li>
+            ))}
+          </ul>
+        </div>
+      ) )}
     </div>
   );
 };
 
 export default Movies;
+
+//title, time, genres, metascore? 
