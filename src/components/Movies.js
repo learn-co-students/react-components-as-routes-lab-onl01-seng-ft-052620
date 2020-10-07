3008f30661a60bbf0e882c38f067be6106964293
nae <nae@JenChrisensiMac.attlocal.net>
2020-10-07 14:31:34 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,38 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        <div>
+          <h2>Doctor Strange</h2>
+          <h3>Time: 115</h3>
+          <ul>
+            <li>Action</li>
+            <li>Adventure</li>
+            <li>Fantasy</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Trolls</h2>
+          <h3>Time: 92</h3>
+          <ul>
+            <li>Animation</li>
+            <li>Adventure</li>
+            <li>Comedy</li>
+            <li>Family</li>
+            <li>Fantasy</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Jack Reacher: Never Go Back</h2>
+          <h3>Time: 118</h3>
+          <ul>
+            <li>Action</li>
+            <li>Adventure</li>
+            <li>Crime</li>
+            <li>Mystery</li>
+            <li>Thriller</li>
+          </ul>
+        </div>
     </div>
   );
 };
